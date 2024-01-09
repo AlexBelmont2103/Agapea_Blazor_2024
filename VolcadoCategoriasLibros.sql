6c2e5c3f20a471e8809d7ea60107e88959d6bb3b
Alejandro Muñoz García <110282968+AlexBelmont2103@users.noreply.github.com>
2024-01-09 11:48:31 +0100

    Añadida consulta para volcado de libros y categorias en la db

---
+++ VolcadoCategoriasLibros.sql
@@ -0,0 +1,32 @@
+﻿SELECT Categorias.* INTO Categorias FROM OPENROWSET(
+BULK 'C:\Users\thard\source\repos\Agapea-Blazor-2024\categorias.json', SINGLE_CLOB
+) AS [Json]
+CROSS APPLY OPENJSON (BulkColumn, '$')
+WITH (
+IdCategoria varchar(100) '$.IdCategoria',
+NombreCategoria varchar(max) '$.NombreCategoria'
+) AS [Categorias]
+
+
+
+SELECT Libros.* INTO Libros FROM OPENROWSET(
+BULK 'C:\Users\thard\source\repos\Agapea-Blazor-2024\libros.json', SINGLE_CLOB
+) AS [Json]
+CROSS APPLY OPENJSON (BulkColumn, '$')
+WITH (
+IdCategoria varchar(100) '$.IdCategoria',
+ImagenLibro varchar(100) '$.ImagenLibro',
+ImagenLibroBASE64 nvarchar(max) '$.ImagenLibroBASE64',
+Titulo varchar(200) '$.Titulo',
+Editorial varchar(200) '$.Editorial',
+Autores varchar(200) '$.Autores',
+Edicion varchar(100) '$.Edicion',
+NumeroPaginas int '$.NumeroPaginas',
+Dimensiones varchar(50) '$.Dimensiones',
+Idioma varchar(50) '$.Idioma',
+ISBN10 varchar(15) '$.ISBN10',
+ISBN13 varchar(20) '$.ISBN13',
+Resumen varchar(max) '$.Resumen',
+Precio decimal(5,2) '$.Precio'
+
+) AS [Libros]
